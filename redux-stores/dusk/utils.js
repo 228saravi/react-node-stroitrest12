cc64be93811b8d7e60066a92191c9968dbdd7b0c
228saravi <228saravi@gmail.com>
2018-10-17 17:21:26 +0300

    qwe

--- redux-stores/dusk/utils.js
+++ redux-stores/dusk/utils.js
@@ -2,8 +2,9 @@ import {OrderedMap, Map} from 'immutable'
 
 
 export function fbDatatoEntities(data, RecordModel = Map) {
-    console.log()
-    return (new OrderedMap(data)).mapEntries(([uid, value]) => (
-        [uid, new RecordModel(value)]
-    ))
+    console.log(data)
+    return (new OrderedMap(data)).mapEntries(([uid, value]) => {
+        console.log(value)
+        return([uid, new RecordModel(value)]
+    )})
 }
